0205b24b6a00d4d54c75c4da57bd826c2e12c327
katipogulaBhaskar <157219070+katipogulaBhaskar@users.noreply.github.com>
2024-10-25 23:03:25 +0530

    Add files via upload

---
+++ week-4/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const { sequelize } = require('./src/models/index');
+const userRoutes = require('./src/routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', userRoutes);
+
+
+sequelize.sync().then(() => {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}).catch(error => {
+    console.log(`Unable to connect to the database:`, error);
+})
+
---
+++ week-4/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+//const { sequelize } = require('./src/models');
+const sequelize = require('./src/config/Config'); 
+const user_Routes = require('./src/routes/user_Routes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', user_Routes);
+
+
+sequelize.sync().then(() => {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}).catch(error => {
+    console.log(`Unable to connect to the database:`, error);
+})
+
